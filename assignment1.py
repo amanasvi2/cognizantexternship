15786b7c0cb2be1900dedd6db14d1d37a5a80097
Archita Manasvi <architamanasvi@gmail.com>
2025-05-08 15:20:19 -0400

    Assignment 1

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+# Task 1: Variables: Your First Python Intro 
+name = "Archita Manasvi" 
+age = 19 
+height = 5.8 
+
+# Example usage
+print(f"Hi my name is {name}. I'm {age} years old and {height} feet tall.")
+
+# Task 2: Operator: Playing with Numbers
+# This task will add two numbers and print the result
+num1 = 10
+num2 = 5
+print("The sum of 10 and 5 is:", num1 + num2)
+
+# Task 3: Conditional Statements: The Number Checker
+# This task will check if a number is positive, negative, or zero
+number = float(input("Enter a number: "))
+if number > 0:
+    print("The number is positive. Great job!")
+elif number < 0:
+    print("The number is negative. Don't worry, it happens!")
+else:
+    print("The number is zero. It's a neutral number!")
